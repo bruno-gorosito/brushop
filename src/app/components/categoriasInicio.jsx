30880d5503a11332ac4781e28974b059aeece141
Bruno Gorosito <gorositobrunoalejandro@gmail.com>
2023-06-06 11:36:35 -0300

    con categorias diferenciadas

--- src/app/components/categoriasInicio.jsx
+++ src/app/components/categoriasInicio.jsx
@@ -17,8 +17,6 @@ export const CategoriasInicio = ({categoria}) => {
 
     
     useEffect(() => {
-        console.log(productos)
-        console.log(categoria)
         filtrarProductos()
     }, [productos])
 
@@ -26,7 +24,7 @@ export const CategoriasInicio = ({categoria}) => {
         <>
             <div className="px-2 w-full max-w-5xl mx-auto my-8">
                 <div>
-                    <h5 className="capitalize after:w-full relative after:bg-gray-400 after:h-0.5 after:absolute hover:after:scale-100 after:top-full after:left-0 after:scale-0 after:transition after:duration-300 after:origin-left hover:cursor-pointer">{categoria.attributes.name}</h5>
+                    <h5 className="capitalize pt-4 my-4 after:w-full relative after:bg-gray-400 after:h-0.5 after:absolute hover:after:scale-100 after:top-full after:left-0 after:scale-0 after:transition after:duration-300 after:origin-left hover:cursor-pointer">{categoria.attributes.name}</h5>
                 </div>
                 <div className="flex ">
                     {productosPorCategoria.length !== 0 
